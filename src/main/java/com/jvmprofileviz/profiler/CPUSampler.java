7fdab1f9fac5e2b2712d2e780067d646c82ceb6a
Emmanuel Papirakis <epapirakis@zulily.com>
2019-03-14 20:51:19 -0700

    Fixed a bug

--- src/main/java/com/jvmprofileviz/profiler/CPUSampler.java
+++ src/main/java/com/jvmprofileviz/profiler/CPUSampler.java
@@ -131,7 +131,7 @@ public class CPUSampler {
                 }
             }
 
-            threadCPUTime.put(ti.getThreadId(), cpuTime);
+            threadCPUTime.put(ti.getThreadId(), tCPUTime);
         }
         if (samplesAcquired) {
             updateCount_.incrementAndGet();
