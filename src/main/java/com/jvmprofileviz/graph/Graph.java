60c94159515166dd8b45a4023a52b2a6b09b560a
Emmanuel Papirakis <epapirakis@zulily.com>
2019-03-14 18:39:01 -0700

    Added a simple graph

--- src/main/java/com/jvmprofileviz/graph/Graph.java
+++ src/main/java/com/jvmprofileviz/graph/Graph.java
@@ -1,4 +1,36 @@
 package com.jvmprofileviz.graph;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.HashMap;
+
 public class Graph {
+    private static final ObjectMapper mapper = new ObjectMapper();
+    private final HashMap<String, Vertex> graph = new HashMap<String, Vertex>();
+
+    public void visitLink(String from, String to) {
+        Vertex fromVertex = getVertex(from);
+        fromVertex.addVisit();
+
+        Vertex toVertex = getVertex(to);
+        toVertex.addVisit();
+    }
+
+    private Vertex getVertex(String key) {
+        Vertex vertex;
+
+        if (!graph.containsKey(key)) {
+            vertex = new Vertex();
+            graph.put(key, vertex);
+        } else {
+            vertex = graph.get(key);
+        }
+
+        return vertex;
+    }
+
+    public String serialize() throws JsonProcessingException {
+        return mapper.writeValueAsString(this);
+    }
 }
